2cbf49aee6693552a05b94e92a0fb9e7fac4b17c
Johannes Flodin <jannis020609@gmail.com>
2026-03-02 16:19:23 +0100

    first modal

---
+++ src/scripts/portfolio.js
@@ -0,0 +1,6 @@
+const modal = document.getElementById("projectModal");
+const openModalBtn = document.getElementById("openProjectModal");
+
+openModalBtn.addEventListener("click", () => {
+    modal.showModal();
+});
