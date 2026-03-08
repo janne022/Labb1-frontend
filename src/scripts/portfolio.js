72befe1323c51cffa6f62880d2cf2dd5bd94fcc6
Johannes Flodin <jannis020609@gmail.com>
2026-03-08 22:58:44 +0100

    added close to card

--- src/scripts/portfolio.js
+++ src/scripts/portfolio.js
@@ -1,6 +1,11 @@
 const modal = document.getElementById("projectModal");
 const openModalBtn = document.getElementById("openProjectModal");
+const closeModalBtn = document.getElementById("closeProjectModal");
 
 openModalBtn.addEventListener("click", () => {
     modal.showModal();
+});
+
+closeModalBtn.addEventListener("click", () => {
+    modal.close();
 });
